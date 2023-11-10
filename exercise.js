346bb95f090a07359e9057df3f1f6f26a8e53c11
Kiss Kitti <kiss.kitti.222@gmail.com>
2023-11-10 19:07:35 +0100

    ts task

---
+++ exercise.js
@@ -0,0 +1,78 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const writers2 = [
+    { id: 1, name: "J. K. Rowling" },
+    { id: 2, name: "Stephen King" },
+    { id: 3, name: "Móra Ferenc" }
+];
+const books2 = [
+    { writerId: 1, title: "...", type: "fantasy", numberOfWords: 100000 },
+    { writerId: 1, title: "...", type: "thriller", numberOfWords: 200000 },
+    { writerId: 1, title: "...", type: "scifi", numberOfWords: 150000 },
+    { writerId: 2, title: "...", type: "scifi", numberOfWords: 400000 },
+    { writerId: 2, title: "...", type: "fantasy", numberOfWords: 300000 },
+    { writerId: 3, title: "...", type: "fantasy", numberOfWords: 300000 },
+    { writerId: 3, title: "...", type: "fantasy", numberOfWords: 300000 },
+];
+const result2 = getAmountPublishedInFantasy(writers2, books2, "J. K. Rowling");
+console.log(result2 === 100000);
+const writers3 = [
+    { id: 1, name: "J. K. Rowling" },
+    { id: 2, name: "Stephen King" },
+    { id: 3, name: "Móra Ferenc" }
+];
+const books3 = [
+    { writerId: 1, title: "...", type: "fantasy", numberOfWords: 100000 },
+    { writerId: 1, title: "...", type: "thriller", numberOfWords: 200000 },
+    { writerId: 1, title: "...", type: "scifi", numberOfWords: 150000 },
+    { writerId: 2, title: "...", type: "scifi", numberOfWords: 400000 },
+    { writerId: 2, title: "...", type: "fantasy", numberOfWords: 300000 },
+    { writerId: 3, title: "...", type: "fantasy", numberOfWords: 300000 },
+    { writerId: 3, title: "...", type: "fantasy", numberOfWords: 300000 },
+];
+const result3 = getAmountPublishedInFantasy(writers3, books3, "Stephen King");
+console.log(result3 === 300000);
+const writers4 = [
+    { id: 1, name: "J. K. Rowling" },
+    { id: 2, name: "Stephen King" },
+    { id: 3, name: "Móra Ferenc" }
+];
+const books4 = [
+    { writerId: 1, title: "...", type: "fantasy", numberOfWords: 100000 },
+    { writerId: 1, title: "...", type: "thriller", numberOfWords: 200000 },
+    { writerId: 1, title: "...", type: "scifi", numberOfWords: 150000 },
+    { writerId: 2, title: "...", type: "scifi", numberOfWords: 400000 },
+    { writerId: 2, title: "...", type: "thriller", numberOfWords: 300000 },
+    { writerId: 3, title: "...", type: "fantasy", numberOfWords: 300000 },
+    { writerId: 3, title: "...", type: "fantasy", numberOfWords: 300000 },
+];
+const result4 = getAmountPublishedInFantasy(writers4, books4, "Stephen King");
+console.log(result4 === 0);
+const writers1 = [
+    { id: 1, name: "J. K. Rowling" },
+    { id: 2, name: "Stephen King" },
+    { id: 3, name: "Móra Ferenc" }
+];
+const books1 = [
+    { writerId: 1, title: "...", type: "fantasy", numberOfWords: 100000 },
+    { writerId: 1, title: "...", type: "thriller", numberOfWords: 200000 },
+    { writerId: 1, title: "...", type: "fantasy", numberOfWords: 150000 },
+    { writerId: 2, title: "...", type: "scifi", numberOfWords: 400000 },
+    { writerId: 2, title: "...", type: "fantasy", numberOfWords: 300000 },
+    { writerId: 3, title: "...", type: "fantasy", numberOfWords: 300000 },
+    { writerId: 3, title: "...", type: "fantasy", numberOfWords: 300000 },
+];
+/* ---CODE---- */
+function getAmountPublishedInFantasy(writers, books, writerName) {
+    const writer = writers.find((writer) => writer.name === writerName);
+    if (writer === undefined) {
+        return 0;
+    }
+    let writerBooks = books.filter(book => book.writerId === writer.id);
+    let wordCount = writerBooks.reduce((sum, book) => {
+        return book.type === "fantasy" ? sum + book.numberOfWords : sum;
+    }, 0);
+    return wordCount;
+}
+const result1 = getAmountPublishedInFantasy(writers1, books1, "J. K. Rowling");
+console.log(result1 === 25000);
